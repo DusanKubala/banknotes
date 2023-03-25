abb2898f37f8c160713c0559402dd4dd47e40f97
Dussky <dusankubala1996@gmail.com>
2023-03-25 18:49:52 +0100

    Cleanup project

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <banknote-card></banknote-card>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BanknoteCard from "./components/BanknoteCard.vue"
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: [
+    BanknoteCard
+  ]
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import BanknoteCard from "./components/BanknoteCard.vue"
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.component('banknote-card', BanknoteCard);
+
+app.mount('#app')
